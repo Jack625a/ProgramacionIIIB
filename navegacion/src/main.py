827029e6a684013a9cd4a9334adb1a4a37c96342
Jack625a <ktfut15@gmail.com>
2025-05-07 10:16:05 -0400

    BARRA NAVEGACION LATERAL

--- navegacion/src/main.py
+++ navegacion/src/main.py
@@ -1,11 +1,14 @@
 import flet as ft
-
+#MODIFICACIONES
 
 def main(page: ft.Page):
+
+
+
     page.title="Navegacion"
     #APP BAR CABECERAS
     page.appbar=ft.CupertinoAppBar(
-        leading=ft.Icon(ft.icons.MENU),
+        leading=ft.Icon(ft.Icons.MENU),
         middle=ft.Text("Navegaciones"),
         bgcolor=ft.Colors.RED_400
     )
@@ -13,16 +16,40 @@ def main(page: ft.Page):
     #NAVEGACION
     page.navigation_bar=ft.NavigationBar(
         destinations=[
-            ft.NavigationBarDestination(label="Inicio",icon=ft.icons.HOME),
-            ft.NavigationBarDestination(label="Ajustes",icon=ft.icons.SETTINGS),
-            ft.NavigationBarDestination(label="Perfil",icon=ft.icons.PERSON),
-            ft.NavigationBarDestination(label="Servicios", icon=ft.icons.ROOM_SERVICE)
-        ],bgcolor=ft.colors.RED_800, indicator_color=ft.colors.WHITE
+            ft.NavigationBarDestination(label="Inicio",icon=ft.Icons.HOME),
+            ft.NavigationBarDestination(label="Ajustes",icon=ft.Icons.SETTINGS),
+            ft.NavigationBarDestination(label="Perfil",icon=ft.Icons.PERSON),
+            ft.NavigationBarDestination(label="Servicios", icon=ft.Icons.ROOM_SERVICE)
+        ],bgcolor=ft.Colors.RED_800, indicator_color=ft.Colors.WHITE
     )
 
+
+    def cerrar(e):
+        page.close(navegacionLateral)
+
+    
+    #NAVEGACION LATERAL 
+    navegacionLateral=ft.NavigationDrawer(
+        controls=[
+            ft.NavigationDrawerDestination(
+                label="Opcion1",
+                icon=ft.Icon(ft.Icons.HOME_MAX)
+            ),
+            ft.Divider(height=1),
+            ft.NavigationDrawerDestination(
+                label="Opcion2",
+                icon=ft.Icon(ft.Icons.AC_UNIT_OUTLINED) 
+            )
+        ],
+        position=ft.NavigationDrawerPosition.END,
+        on_change=cerrar,
+    )
+
+    #page.open(navegacionLateral)
    
     page.add(
-        
+       
+        ft.CupertinoButton(text="PRUEBA",on_click=lambda e: page.open(navegacionLateral))
     )
 
 
