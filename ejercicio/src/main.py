7e4f24af5475d7e192c1732db96beb8245ca4123
Jack625a <ktfut15@gmail.com>
2025-06-05 11:54:47 -0400

    Airtable Conexion Flet

--- ejercicio/src/main.py
+++ ejercicio/src/main.py
@@ -1,11 +1,49 @@
 import flet as ft #Importacion del framework
+#PASO 1. IMPORTAR LAS LIBRERIAS
+import requests #peticiones o solicitudes a un servidor
 
+#PASO 2. CONFIGURAR LAS VARIABLES DE CONEXION
+TOKENAIRTABLE=''
+idBasedatos= ''
+nombreTabla='Estudiante'
+
+#PASO 3. CONFIGURAR LA URL DE CONEXION ENDPOINT
+url=f"https://api.airtable.com/v0/{idBasedatos}/{nombreTabla}"
+#PASO4. CONFIGURAR LA AUTENTIFICACION
+HEADERS={
+    "Authorization": f"Bearer {TOKENAIRTABLE}"
+}
+
+#PASO 5. CREAR LA FUNCION PARA OBTENER LOS DATOS
 def obtencionDatos():
-    pass
+    try:
+        respuesta=requests.get(url,headers=HEADERS)
+        #VERIFICAR SI SE CONECTA CON EL SERVIDOR
+        if respuesta.status_code==200:
+            registros=respuesta.json().get("records",[])
+            return registros
+        else:
+            print("Error al obtener los datos...")
+    except Exception as e:
+        print("Error en las credenciales ",e)
+        return []
 
 #Funcion principal
 def main(page: ft.Page): #ft.Page: Crear una ventana 
-
+    def mostrarDatos():
+        datos=obtencionDatos()
+        print(datos)
+        textoEstudiante=("Lista de Estudiante: ")
+        for dato in datos:
+            registros=dato.get("Nombre","No tiene nombre"),
+            carrera=dato.get("Carrera","Sin carrera"),
+            celular=dato.get("Celular","Sin celular"),
+            edad=dato.get("Edad","0"),
+        
+        textoEstudiante+=f"{registros} - {carrera} - Edad: {edad} - Celular: {celular}"
+        page.update()
+    
+   
     page.title="Prueba"
     page.appbar=ft.CupertinoAppBar(
         middle=ft.Text("Titulo Aplicacion", size=20,color=ft.Colors.GREEN_900)
@@ -14,7 +52,8 @@ def main(page: ft.Page): #ft.Page: Crear una ventana
         destinations=[
             ft.NavigationBarDestination(
                 label="Opcion 1",
-                icon=ft.Icons.READ_MORE_SHARP
+                icon=ft.Icons.READ_MORE_SHARP,
+                
             ),
             ft.NavigationBarDestination(
                 label="Opcion 2",
@@ -36,20 +75,18 @@ def main(page: ft.Page): #ft.Page: Crear una ventana
     #Componentes separados
     menu=ft.Tabs(
         tabs=[
-            ft.Tab(text="Opcion 1",icon=ft.Icons.PRINT,content=ft.Text("Pantalla opcion 1")),
+            ft.Tab(text="Opcion 1",icon=ft.Icons.PRINT,content= ft.CupertinoFilledButton("Mostrar Estudiante",on_click=mostrarDatos)),
             ft.Tab(text="Opcion 2",icon=ft.Icons.DELETE),
             ft.Tab(text="Opcion 3",icon=ft.Icons.APPS),
             ft.Tab(text="Opcion 4",icon=ft.Icons.FACEBOOK)
         ]
     )
-
+    mostrarDatos()
 
     page.add(  #Metodo add nos permite agregar componentes a la interfaz
       #Componentes basicos
-      menu,
-      ft.Text(value="Componentes"),
-      ft.CupertinoFilledButton("Boton1"),
-      imagen,
+        ft.CupertinoFilledButton("Mostrar Estudiante",on_click=mostrarDatos)
+       
 
 
     )
@@ -57,6 +94,3 @@ def main(page: ft.Page): #ft.Page: Crear una ventana
 
 ft.app(main) 
 
-
-
-lista=[1,3,4,6]
